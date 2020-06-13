73ace4f32c12fbec618ad701ea3610a6c38a80c5
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-13 17:45:21 +0000

    create static data

---
+++ frontend/src/data.js
@@ -0,0 +1,36 @@
+  const data = {
+  products:[
+    {
+       name: "Tire",
+       category: "Tires",
+       image: "/images/tire.jpg",
+       price: 60,
+       brand: "Kia",
+       rating: 4.5,
+       numReviews: 10,
+       year: 2018
+    },
+    {
+       name: "Head Light",
+       category: "Lights",
+       image: "/images/tire.jpg",
+        price: 40,
+        brand: "Kia",
+        rating: 4.5,
+        numReviews: 10,
+        year: 2020
+    },
+    {
+       name: "Tail Light ",
+       category: "Lights",
+       image: "/images/tire.jpg",
+        price: 45,
+        brand: "Kia",
+        rating: 4.5,
+        numReviews: 10,
+        year: 2019
+    },
+  ]
+}
+
+export default data;
