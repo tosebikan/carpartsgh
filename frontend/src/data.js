94a9e5a9f6fef10776493d72d0816f49d0f742b3
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-23 08:35:34 +0000

    static datat update with ids

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,6 +1,7 @@
   const data = {
   products:[
     {
+      _id: '100',
        name: "Tire",
        category: "Tires",
        image: "/images/tire.jpg",
@@ -11,6 +12,7 @@
        year: 2018
     },
     {
+      _id: '101',
        name: "Head Light",
        category: "Lights",
        image: "/images/head-light.jpg",
@@ -21,6 +23,7 @@
         year: 2020
     },
     {
+       _id: '110',
        name: "Tail Light ",
        category: "Lights",
        image: "/images/tail-light.jpg",
