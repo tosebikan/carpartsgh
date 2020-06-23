91ff8d97501f5e6494a96577b215ab661f299ad4
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-06-23 08:36:31 +0000

    links

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import data from '../data.js';
 
 const HomeScreen = (props) => (
   <ul className="products">
   {data.products.map((product) =>
-    <li>
-      <div className="product">
+    <li key={product._id}>
+      <div className="product" >
+      <Link to={'/product/' + product._id}>
         <img className="product-image"src={product.image} alt="product"/>
+        </Link>
         <div className="product-name">
-          <a href="product.html">{product.name}</a>
+          <Link to={'/product/' + product._id}>{product.name}</Link>
         </div>
         <div className="product-brand">{product.brand}</div>
         <div className="product-price">${product.price}</div>
