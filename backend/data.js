dac5edc968fc92a1adfe50f75b9465b2c9505e07
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-02 23:54:52 +0000

    backend data

---
+++ backend/data.js
@@ -0,0 +1,39 @@
+  const data = {
+  products:[
+    {
+      _id: '100',
+       name: "Tire",
+       category: "Tires",
+       image: "/images/tire.jpg",
+       price: 60,
+       brand: "Kia",
+       rating: 4.5,
+       numReviews: 10,
+       year: 2018
+    },
+    {
+      _id: '101',
+       name: "Head Light",
+       category: "Lights",
+       image: "/images/head-light.jpg",
+        price: 40,
+        brand: "Kia",
+        rating: 4.5,
+        numReviews: 10,
+        year: 2020
+    },
+    {
+       _id: '110',
+       name: "Tail Light ",
+       category: "Lights",
+       image: "/images/tail-light.jpg",
+        price: 45,
+        brand: "Kia",
+        rating: 4.5,
+        numReviews: 10,
+        year: 2019
+    },
+  ]
+}
+
+export default data;
