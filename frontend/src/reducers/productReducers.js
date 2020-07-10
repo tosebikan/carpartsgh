1e95f08c3a1f38fb14a10f7a4ca70f88472f4ecb
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-10 00:46:20 +0000

    product reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,14 +1,33 @@
+import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAILED, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAILED} from '../constants/productConstants'
+
 const productListReducer = (state ={products: []}, action) => {
+  console.log(action)
+  console.log(state)
   switch(action.type){
-    case "PRODUCT_LIST_REQUEST":
+    case PRODUCT_LIST_REQUEST:
     return {loading: true}
-    case "PRODUCT_LIST_SUCCESS":
+    case PRODUCT_LIST_SUCCESS:
     return{loading: false, products: action.payload}
-    case "PRODUCT_LIST_FAILED":
+    case PRODUCT_LIST_FAILED:
     return{loading: false, error: action.payload}
     default:
     return state;
   }
 }
 
-export {productListReducer};
+const productDetailsReducer = (state ={product: {}}, action) => {
+  console.log(action)
+  console.log(state)
+  switch(action.type){
+    case PRODUCT_DETAILS_REQUEST:
+    return {loading: true}
+    case PRODUCT_DETAILS_SUCCESS:
+    return{loading: false, product: action.payload}
+    case PRODUCT_DETAILS_FAILED:
+    return{loading: false, error: action.payload}
+    default:
+    return state;
+  }
+}
+
+export {productListReducer, productDetailsReducer};
