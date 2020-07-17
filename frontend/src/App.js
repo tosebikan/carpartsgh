96c0e4060536f3a7e263414e61bbdbd4b1d60cee
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-17 18:11:55 +0000

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,59 +3,60 @@ import {BrowserRouter, Route, Link} from 'react-router-dom';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   const openMenu = () =>{
-    document.querySelector(".sidebar").classList.add("open")
-  }
+    document.querySelector(".sidebar").classList.add("open");
+  };
 
   const closeMenu =() => {
-    document.querySelector(".sidebar").classList.remove("open")
-  }
+    document.querySelector(".sidebar").classList.remove("open");
+  };
 
   return (
     <BrowserRouter>
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <button className="brand-button" onClick={openMenu}>&#9776;</button>
-          <Link to="/">CarpartsGh</Link>
-        </div>
-        <div className="header-links ">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Signin</a>
-          <a href="signup.html">Signup</a>
-        </div>
-      </header>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button className="brand-button" onClick={openMenu}>&#9776;</button>
+            <Link to="/">CarpartsGh</Link>
+          </div>
+          <div className="header-links ">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Signin</a>
+            <a href="signup.html">Signup</a>
+          </div>
+        </header>
 
-      <aside className="sidebar">
-        <h3>Select Car Brand</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-        <ul>
-          <li>
-          <a href="index.html">Kia</a>
-          </li>
-          <li>
-          <a href="index.html">Toyota</a>
-        </li>
-          <li>
-          <a href="index.html">Mercedes Benz</a>
-        </li>
-        </ul>
+        <aside className="sidebar">
+          <h3>Select Car Brand</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <ul>
+            <li>
+              <a href="index.html">Kia</a>
+            </li>
+            <li>
+              <a href="index.html">Toyota</a>
+            </li>
+            <li>
+              <a href="index.html">Mercedes Benz</a>
+            </li>
+          </ul>
 
-      </aside>
-      <main className="main">
-        <div className="content">
-
-        <Route path="/product/:id" component={ProductScreen}/>
-        <Route path="/" exact={true} component={HomeScreen} />
-
-      </div>
-      </main>
-      <footer className="footer">
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/product/:id" component={ProductScreen}/>
+            <Route path="/" exact={true} component={HomeScreen} />
+            <Route path='/cart/:id?'
+              component={CartScreen}/>
+          </div>
+        </main>
+        <footer className="footer">
         2020 oltostudios All Rights Reserved
-      </footer>
-    </div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,39 +7,40 @@ import {listProducts} from '../actions/productActions';
 
 const HomeScreen = (props) => {
 //  const [products, setProducts] = useState([])
-  const productList = useSelector(state => state.productList)
+  const productList = useSelector((state) => state.productList);
   const {products, loading, error } = productList;
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(listProducts())
+    dispatch(listProducts());
     {/*const fetchData = async () => {
       const {data} = await axios.get("/api/products")
       setProducts(data)
     }
     fetchData()*/}
-  },[])
+  },[]);
   return(
     loading ? <div>Loading...</div> :
-    error ? <div>{error}</div> :
-  <ul className="products">
-  {products.map((product) =>
-    <li key={product._id}>
-      <div className="product" >
-      <Link to={'/product/' + product._id}>
-        <img className="product-image" src={product.image} alt="product"/>
-        </Link>
-        <div className="product-name">
-          <Link to={'/product/' + product._id}>{product.name}</Link>
-        </div>
-        <div className="product-brand">{product.brand}</div>
-        <div className="product-price">${product.price}</div>
-        <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
-        <div className="product-year">{product.year}</div>
-      </div>
-    </li>
-  )}
-  </ul>
-)}
+      error ? <div>{error}</div> :
+        <ul className="products">
+          {products.map((product) =>
+            <li key={product._id}>
+              <div className="product" >
+                <Link to={'/product/' + product._id}>
+                  <img className="product-image" src={product.image} alt="product"/>
+                </Link>
+                <div className="product-name">
+                  <Link to={'/product/' + product._id}>{product.name}</Link>
+                </div>
+                <div className="product-brand">{product.brand}</div>
+                <div className="product-price">${product.price}</div>
+                <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
+                <div className="product-year">{product.year}</div>
+              </div>
+            </li>
+          )}
+        </ul>
+  );
+};
 
 export default HomeScreen;
