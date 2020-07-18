38535952853be3bd59a0debb076a4f18c52a8c5a
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-18 12:35:52 +0000

    constants update & error fix

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import {ADD_TO_CART} from '../constants/cartConstants';
+import {CART_ADD_ITEM} from '../constants/cartConstants';
 
 const addToCart = (productId, qty) => async (dispatch)=> {
   try{
-    const {data} = await axios.get('/api/rpoducts/'+productId);
-    dispatch({ type: ADD_TO_CART, payload: {
+    const {data} = await axios.get('/api/products/' +productId);
+    dispatch({ type: CART_ADD_ITEM, payload: {
       product: data._id,
       name: data.name,
       image: data.image,
