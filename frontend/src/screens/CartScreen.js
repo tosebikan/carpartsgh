d7b98eae6321060d7ae111f33a44a4021e8b6363
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-18 12:33:06 +0000

    update

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,17 +1,18 @@
 import React,{useEffect} from 'react';
 import {useDispatch} from 'react-redux';
+import {addToCart} from '../actions/cartActions';
 
 
 const CartScreen = (props) => {
-  const productId = props.match.params.id
+  const productId = props.match.params.id;
   const qty = props.location.search ? Number(props.location.search.split("=")[1]) : 1;
   const dispatch = useDispatch();
 
   useEffect(() => {
     if(productId){
-      dispatch(addToCart(productId, qty))
+      dispatch(addToCart(productId, qty));
     }
-  },[])
+  },[]);
   return(
     <div>CartScreen</div>
   );
