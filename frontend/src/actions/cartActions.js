68ee1888057738c7ae03fa877d39db7a92071031
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-20 12:26:43 +0000

    add cart items to cookie

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import Cookie from 'js-cookie';
 import {CART_ADD_ITEM, CART_REMOVE_ITEM} from '../constants/cartConstants';
 
-const addToCart = (productId, qty) => async (dispatch)=> {
+const addToCart = (productId, qty) => async (dispatch, getState)=> {
   try{
     const {data} = await axios.get('/api/products/' +productId);
     dispatch({ type: CART_ADD_ITEM, payload: {
@@ -12,13 +13,18 @@ const addToCart = (productId, qty) => async (dispatch)=> {
       countInStock: data.countInStock,
       qty
     }});
+    const {cart: {cartItems}} = getState();
+    Cookie.set("cartItems", JSON.stringify(cartItems));
   }catch(error){
 
   }
 };
 
-const removeFromCart = (productId) => (dispatch) => {
+const removeFromCart = (productId) => (dispatch, getState) => {
   dispatch({type: CART_REMOVE_ITEM, payload: productId});
+
+  const {cart: {cartItems}} = getState();
+  Cookie.set("cartItems", JSON.stringify(cartItems));
 };
 
 export{
