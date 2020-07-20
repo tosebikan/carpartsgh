b7a5a3c8ebed83fddc562eb96c0f28c79fdae4a2
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-20 11:50:12 +0000

    remove from cart action

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -17,6 +17,11 @@ const addToCart = (productId, qty) => async (dispatch)=> {
   }
 };
 
+const removeFromCart = (productId) => (dispatch) => {
+
+};
+
 export{
-  addToCart
+  addToCart,
+  removeFromCart
 };
