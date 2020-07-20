0d10347031acc9bd68cb151d2fc1472f65e0de41
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-20 11:56:59 +0000

    remove item action

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {CART_ADD_ITEM} from '../constants/cartConstants';
+import {CART_ADD_ITEM, CART_REMOVE_ITEM} from '../constants/cartConstants';
 
 const addToCart = (productId, qty) => async (dispatch)=> {
   try{
@@ -18,7 +18,7 @@ const addToCart = (productId, qty) => async (dispatch)=> {
 };
 
 const removeFromCart = (productId) => (dispatch) => {
-
+  dispatch({type: CART_REMOVE_ITEM, payload: productId});
 };
 
 export{
