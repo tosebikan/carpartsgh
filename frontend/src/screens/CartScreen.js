26aee09f94dc9fc4bb4df049727b8e7d92deca1f
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-20 11:06:29 +0000

    cart screen product add to cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,11 @@
 import React,{useEffect} from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch,useSelector} from 'react-redux';
 import {addToCart} from '../actions/cartActions';
 
 
 const CartScreen = (props) => {
+  const cart = useSelector((state)=> state.cart);
+  const {cartItems} = cart;
   const productId = props.match.params.id;
   const qty = props.location.search ? Number(props.location.search.split("=")[1]) : 1;
   const dispatch = useDispatch();
@@ -13,8 +15,51 @@ const CartScreen = (props) => {
       dispatch(addToCart(productId, qty));
     }
   },[]);
+
   return(
-    <div>CartScreen</div>
+    <div className="cart">
+      <div className="cart-list">
+        <ul className="cart-list-container">
+          <li>
+            <h3>Shopping Cart</h3>
+            <div>Price</div>
+          </li>
+          { cartItems.length === 0 ? <div>Cart is empty</div>
+            :
+            cartItems.map((item) =>
+              <div>
+                <img src={item.image} alt="product"/>
+                <div className="cart-name">
+                  <div>{item.name}</div>
+                  <div>
+                    Qty:
+                    <select>
+                      <option value='1'>1</option>
+                      <option value='2'>2</option>
+                      <option value='3'>3</option>
+                    </select>
+                  </div>
+                </div>
+                <div>{item.price}</div>
+              </div>
+            )
+          }
+
+
+        </ul>
+      </div>
+      <div className="cart-action">
+        <h3>
+        Subtotal ({cartItems.reduce((a,c) => a + c.qty, 0)} items)
+        :
+          {cartItems.reduce((a,c) => a + c.price * c.qty, 0)}
+        </h3>
+        <button className="button primary" disabled={cartItems.length === 0 }>
+          Proceed to checkout
+        </button>
+
+      </div>
+    </div>
   );
 };
 
