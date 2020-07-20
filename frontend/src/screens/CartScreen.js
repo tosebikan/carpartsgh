b4a0921f139b53636faff7b14f045a350dd8ba80
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-20 12:27:26 +0000

    check out implementation

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -22,7 +22,7 @@ const CartScreen = (props) => {
   };
 
   const checkoutHandler = () => {
-
+    props.history.push('/signin?redirect=shipping');
   };
 
   return(
@@ -72,7 +72,7 @@ const CartScreen = (props) => {
         :
           $ {cartItems.reduce((a,c) => a + c.price * c.qty, 0)}
         </h3>
-        <button onClick={checkoutHandler} className="button primary" disabled={cartItems.length === 0 }>
+        <button onClick={checkoutHandler} className="button primary full-width" disabled={cartItems.length === 0 }>
           Proceed to checkout
         </button>
 
