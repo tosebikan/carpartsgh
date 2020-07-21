3f52ec0d7bc350986e25a86b6c8e161de5ed560b
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-21 12:28:12 +0000

    mongo db added

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,34 @@
-import express from 'express';
-import data from './data';
+import express from "express";
+import data from "./data";
+import dotenv from "dotenv";
+import config from "./config";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true
+  })
+  .catch(error => console.log(error.reason));
 
 const app = express();
 
 app.get("/api/products/:id", (req, res) => {
   const productId = req.params.id;
-  const product = data.products.find(x => x._id === productId)
-  if(product){
+  const product = data.products.find(x => x._id === productId);
+  if (product) {
     res.send(product);
-  }else{
-    res.status(404).send({msg: 'Product not found.'})
+  } else {
+    res.status(404).send({ msg: "Product not found." });
   }
-})
+});
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
-})
+});
 
-app.listen(5000, ()=> { console.log('server running at http://localhost:5000')})
+app.listen(5000, () => {
+  console.log("server running at http://localhost:5000");
+});
