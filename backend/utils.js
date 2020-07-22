8959b8f206c8a870c34e9b964ebb8f79fa884868
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-22 12:26:08 +0000

    get token function

---
+++ backend/utils.js
@@ -0,0 +1,7 @@
+import jwt from "jsonwebtoken";
+import config from "./config";
+const getToken = user => {
+  return jwt.sign(user, config.JWT_SECRET, { expiresIn: "48h" });
+};
+
+export default getToken;
