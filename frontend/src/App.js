989ae71ba203371ebf5326081701940ad25dade9
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-22 12:26:20 +0000

    sign in setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
-import './App.css';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import SigninScreen from "./screens/SigninScreen";
 
 function App() {
-  const openMenu = () =>{
+  const openMenu = () => {
     document.querySelector(".sidebar").classList.add("open");
   };
 
-  const closeMenu =() => {
+  const closeMenu = () => {
     document.querySelector(".sidebar").classList.remove("open");
   };
 
@@ -19,19 +20,23 @@ function App() {
       <div className="grid-container">
         <header className="header">
           <div className="brand">
-            <button className="brand-button" onClick={openMenu}>&#9776;</button>
+            <button className="brand-button" onClick={openMenu}>
+              &#9776;
+            </button>
             <Link to="/">CarpartsGh</Link>
           </div>
           <div className="header-links ">
             <a href="cart.html">Cart</a>
-            <a href="signin.html">Signin</a>
+            <Link to="/signin">Signin</Link>
             <a href="signup.html">Signup</a>
           </div>
         </header>
 
         <aside className="sidebar">
           <h3>Select Car Brand</h3>
-          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
           <ul>
             <li>
               <a href="index.html">Kia</a>
@@ -43,19 +48,16 @@ function App() {
               <a href="index.html">Mercedes Benz</a>
             </li>
           </ul>
-
         </aside>
         <main className="main">
           <div className="content">
-            <Route path="/product/:id" component={ProductScreen}/>
+            <Route path="/signin" component={SigninScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
             <Route path="/" exact={true} component={HomeScreen} />
-            <Route path='/cart/:id?'
-              component={CartScreen}/>
+            <Route path="/cart/:id?" component={CartScreen} />
           </div>
         </main>
-        <footer className="footer">
-        2020 oltostudios All Rights Reserved
-        </footer>
+        <footer className="footer">2020 oltostudios All Rights Reserved</footer>
       </div>
     </BrowserRouter>
   );
