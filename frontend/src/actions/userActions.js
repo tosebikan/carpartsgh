9473678e3cf7eddc287e7ebbb3a3208a89321ffd
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-22 13:40:26 +0000

    update

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,7 +2,8 @@ import Axios from "axios";
 import Cookie from "js-cookie";
 import {
   USER_SIGNIN_REQUEST,
-  USER_SIGNIN_SUCCESS
+  USER_SIGNIN_SUCCESS,
+  USER_SIGNIN_FAIL
 } from "../constants/signinConstants";
 
 const signin = (email, password) => async (dispatch) => {
@@ -11,7 +12,9 @@ const signin = (email, password) => async (dispatch) => {
     const { data } = await Axios.post("/api/users/signin", { email, password });
     dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
     Cookie.set("userInfo", JSON.stringify(data));
-  } catch (error) {}
+  } catch (error) {
+    dispatch({ type: USER_SIGNIN_FAIL, payload: error.message });
+  }
 };
 
 export { signin };
