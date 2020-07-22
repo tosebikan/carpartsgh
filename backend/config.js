d7e1495f0351547c759338e2981870b192380cf6
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-22 12:24:34 +0000

    user schema

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export default {
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/carparts"
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/carparts",
+  JWT_SECRET: process.env.JWT_SECRET || "somethingsecret"
 };
