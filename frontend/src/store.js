5be7969ebce928fcc68ad0f49568fd07d906c452
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-22 13:41:56 +0000

    added userSignIn reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,20 +1,28 @@
-import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import Cookie from 'js-cookie';
-import {productListReducer, productDetailsReducer} from './reducers/productReducers';
-import {cartReducer} from './reducers/cartReducers';
-
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import Cookie from "js-cookie";
+import {
+  productListReducer,
+  productDetailsReducer
+} from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
+import { userSigninReducer } from "./reducers/userSigninReducer";
 
 const cartItems = Cookie.getJSON("cartItems") || [];
-const initialState={ cart: {cartItems}};
+const initialState = { cart: { cartItems } };
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  userSignin: userSigninReducer
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
 
 export default store;
