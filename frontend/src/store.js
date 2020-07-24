f92acf50f66cf9e51aa726a6f292b511d190a550
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-24 09:03:26 +0000

    get user info from cookies

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,7 +9,9 @@ import { cartReducer } from "./reducers/cartReducers";
 import { userSigninReducer } from "./reducers/userReducers";
 
 const cartItems = Cookie.getJSON("cartItems") || [];
-const initialState = { cart: { cartItems } };
+const userInfo = Cookie.getJSON("userInfo") || null;
+
+const initialState = { cart: { cartItems }, userSignin: { userInfo } };
 
 const reducer = combineReducers({
   productList: productListReducer,
