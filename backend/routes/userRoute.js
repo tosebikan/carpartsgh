b259b3c6f0c372dfbe26ffd3a5d1f4cf94472deb
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-24 11:39:00 +0000

    register route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -22,6 +22,27 @@ router.post("/signin", async (req, res) => {
   }
 });
 
+router.post("/register", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+  const newUser = await user.save();
+  if (newUser) {
+    res.send({
+      _id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      password: newUser.password,
+      isAdmin: newUser.isAdmin,
+      token: getToken(newUser)
+    });
+  } else {
+    res.status(401).send({ msg: "Invalid User Data" });
+  }
+});
+
 router.get("/createAdmin", async (req, res) => {
   try {
     const user = new User({
