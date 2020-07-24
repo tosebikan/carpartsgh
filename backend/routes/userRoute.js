ab87758e31c2892265c87b7afa0c67d3a9f234b9
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-24 09:04:41 +0000

    signin token

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,10 +4,10 @@ import getToken from "../utils";
 
 const router = express.Router();
 
-router.post("signin", async (req, res) => {
+router.post("/signin", async (req, res) => {
   const signinUser = await User.findOne({
     email: req.body.email,
-    password: req.body.email
+    password: req.body.password
   });
   if (signinUser) {
     res.send({
@@ -15,7 +15,7 @@ router.post("signin", async (req, res) => {
       name: signinUser.name,
       email: signinUser.email,
       isAdmin: signinUser.isAdmin,
-      token: getToken(user)
+      token: getToken(signinUser)
     });
   } else {
     res.status(401).send({ msg: "Invalid email or password" });
@@ -26,7 +26,7 @@ router.get("/createAdmin", async (req, res) => {
   try {
     const user = new User({
       name: "Tolu",
-      email: "oltostudios@gmail.com",
+      email: "tosebikan@gmail.com",
       password: "1234",
       isAdmin: true
     });
