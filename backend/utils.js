441dc3bee9c14d86af3b41d4050ef2e5230c3799
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-26 13:10:45 +0000

    check for authentication & if user is admin

--- backend/utils.js
+++ backend/utils.js
@@ -13,4 +13,27 @@ const getToken = user => {
   );
 };
 
+const isAuth = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    const onlyToken = token.slice(7, token.length);
+    jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => {
+      if (err) {
+        return res.status(401).send({ msg: "Invalid Token" });
+      }
+      req.user = token;
+      next();
+      return;
+    });
+  }
+  return res.status(401).send({ msg: "Token is now supplied" });
+};
+
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    return next();
+  }
+  return res.status(401).send({ msg: "Admin Token Invalid" });
+};
+
 export default getToken;
