f82d8abdf67fea3e9354b7cf70def693aacacfe9
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-26 13:55:00 +0000

    get & post product router

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,32 @@
+import express from "express";
+import Product from "../models/productModel";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const products = await Product.find({});
+  res.send(products);
+});
+
+router.post("/", async (req, res) => {
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    price: req.body.price,
+    brand: req.body.brand,
+    category: req.body.category,
+    countInStock: req.body.countInStock,
+    description: req.body.description,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews
+  });
+  const newProduct = product.save();
+  if (newProduct) {
+    return res
+      .status(201)
+      .send({ message: "New Product Created", data: newProduct });
+  }
+  return res.status(500).send({ message: "Error in creating product" });
+});
+
+export default router;
