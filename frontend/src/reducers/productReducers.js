4f0e34febd89913a6e2a3b6152769dc68d5b68ee
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-28 12:00:07 +0000

    product list state to empty products

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -13,7 +13,7 @@ import {
 const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
-      return { loading: true };
+      return { loading: true, products: [] };
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAILED:
