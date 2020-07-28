85feb01d8b05213ef1217999acb0c5ee77175cf2
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-28 10:24:24 +0000

    product save reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,33 +1,52 @@
-import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAILED, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAILED} from '../constants/productConstants'
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAILED,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAILED,
+  PRODUCT_SAVE_REQUEST,
+  PRODUCT_SAVE_SUCCESS,
+  PRODUCT_SAVE_FAILED
+} from "../constants/productConstants";
 
-const productListReducer = (state ={products: []}, action) => {
-  console.log(action)
-  console.log(state)
-  switch(action.type){
+const productListReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
     case PRODUCT_LIST_REQUEST:
-    return {loading: true}
+      return { loading: true };
     case PRODUCT_LIST_SUCCESS:
-    return{loading: false, products: action.payload}
+      return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAILED:
-    return{loading: false, error: action.payload}
+      return { loading: false, error: action.payload };
     default:
-    return state;
+      return state;
   }
-}
+};
 
-const productDetailsReducer = (state ={product: {}}, action) => {
-  console.log(action)
-  console.log(state)
-  switch(action.type){
+const productSaveReducer = (state = { products: [] }, action) => {
+  switch (action.payload) {
+    case PRODUCT_SAVE_REQUEST:
+      return { loading: true };
+    case PRODUCT_SAVE_SUCCESS:
+      return { loading: false, products: action.payload };
+    case PRODUCT_SAVE_FAILED:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+const productDetailsReducer = (state = { product: {} }, action) => {
+  switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
-    return {loading: true}
+      return { loading: true };
     case PRODUCT_DETAILS_SUCCESS:
-    return{loading: false, product: action.payload}
+      return { loading: false, product: action.payload };
     case PRODUCT_DETAILS_FAILED:
-    return{loading: false, error: action.payload}
+      return { loading: false, error: action.payload };
     default:
-    return state;
+      return state;
   }
-}
+};
 
-export {productListReducer, productDetailsReducer};
+export { productListReducer, productSaveReducer, productDetailsReducer };
