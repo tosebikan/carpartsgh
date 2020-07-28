4946d6211abfc97fd52e206e659363e6bef14a9c
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-28 10:13:36 +0000

    save product action

--- frontend/src/screens/ProductsScreen.js
+++ frontend/src/screens/ProductsScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { saveProduct } from "../actions/cartActions.js";
 
 const ProductScreen = (props) => {
   const [name, setName] = useState("");
