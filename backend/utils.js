5aef1e6bdea4257a96c5152c52f029ed9bdb6808
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-30 23:41:15 +0000

    jwt auth update

--- backend/utils.js
+++ backend/utils.js
@@ -16,20 +16,22 @@ const getToken = user => {
 const isAuth = (req, res, next) => {
   const token = req.headers.authorization;
   if (token) {
-    const onlyToken = token.slice(7, token.length);
+    const onlyToken = token.slice(6, token.length);
     jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => {
       if (err) {
         return res.status(401).send({ msg: "Invalid Token" });
       }
-      req.user = token;
+      req.user = decode;
       next();
       return;
     });
+  } else {
+    return res.status(401).send({ msg: "Token is not supplied" });
   }
-  return res.status(401).send({ msg: "Token is now supplied" });
 };
 
 const isAdmin = (req, res, next) => {
+  console.log(req.user);
   if (req.user && req.user.isAdmin) {
     return next();
   }
