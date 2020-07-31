cdf2871c9e783acdae29029ef7c9f2ce6f726c8c
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-31 00:37:02 +0000

    save shipping action

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,33 +1,43 @@
-import axios from 'axios';
-import Cookie from 'js-cookie';
-import {CART_ADD_ITEM, CART_REMOVE_ITEM} from '../constants/cartConstants';
+import axios from "axios";
+import Cookie from "js-cookie";
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING
+} from "../constants/cartConstants";
 
-const addToCart = (productId, qty) => async (dispatch, getState)=> {
-  try{
-    const {data} = await axios.get('/api/products/' +productId);
-    dispatch({ type: CART_ADD_ITEM, payload: {
-      product: data._id,
-      name: data.name,
-      image: data.image,
-      price: data.price,
-      countInStock: data.countInStock,
-      qty
-    }});
-    const {cart: {cartItems}} = getState();
+const addToCart = (productId, qty) => async (dispatch, getState) => {
+  try {
+    const { data } = await axios.get("/api/products/" + productId);
+    dispatch({
+      type: CART_ADD_ITEM,
+      payload: {
+        product: data._id,
+        name: data.name,
+        image: data.image,
+        price: data.price,
+        countInStock: data.countInStock,
+        qty
+      }
+    });
+    const {
+      cart: { cartItems }
+    } = getState();
     Cookie.set("cartItems", JSON.stringify(cartItems));
-  }catch(error){
-
-  }
+  } catch (error) {}
 };
 
 const removeFromCart = (productId) => (dispatch, getState) => {
-  dispatch({type: CART_REMOVE_ITEM, payload: productId});
+  dispatch({ type: CART_REMOVE_ITEM, payload: productId });
 
-  const {cart: {cartItems}} = getState();
+  const {
+    cart: { cartItems }
+  } = getState();
   Cookie.set("cartItems", JSON.stringify(cartItems));
 };
 
-export{
-  addToCart,
-  removeFromCart
+const saveShipping = (data) => (dispatch) => {
+  dispatch({ type: CART_SAVE_SHIPPING, payload: data });
 };
+
+export { addToCart, removeFromCart, saveShipping };
