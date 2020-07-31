0833d984956535751646a2b0c95ccb71f629eade
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-07-31 00:37:17 +0000

    save shipping reducer

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,24 +1,31 @@
-import {CART_ADD_ITEM, CART_REMOVE_ITEM} from '../constants/cartConstants';
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING
+} from "../constants/cartConstants";
 
-const cartReducer = (state={cartItems: []}, action) => {
-  switch(action.type){
+const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
       const product = state.cartItems.find((x) => x.product === item.product);
-      if(product){
-        return {cartItems: state.cartItems.map((x) => x.product === product.product ? item : x)
+      if (product) {
+        return {
+          cartItems: state.cartItems.map((x) =>
+            x.product === product.product ? item : x
+          )
         };
       }
-      return { cartItems: [...state.cartItems, item]};
+      return { cartItems: [...state.cartItems, item] };
     case CART_REMOVE_ITEM:
       return {
         cartItems: state.cartItems.filter((x) => x.product !== action.payload)
       };
+    case CART_SAVE_SHIPPING:
+      return { ...state, shipping: action.payload };
     default:
       return state;
   }
 };
 
-export {
-  cartReducer
-};
+export { cartReducer };
