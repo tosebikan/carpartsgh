68d881de88a83424798f51a40f55ae3e214546c6
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-03 12:00:28 +0000

    default state objects for shippind & payment

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -17,7 +17,7 @@ const cartItems = Cookie.getJSON("cartItems") || [];
 const userInfo = Cookie.getJSON("userInfo") || null;
 
 const initialState = {
-  cart: { cartItems },
+  cart: { cartItems, shipping: {}, payment: {} },
   userSignin: { userInfo },
   userRegister: { userInfo }
 };
