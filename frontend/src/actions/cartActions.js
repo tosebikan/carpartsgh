71356c38bb1208ffd1d4c8cf334ab4e7ad78d4ba
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-03 10:54:55 +0000

    save payment action added

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -3,7 +3,8 @@ import Cookie from "js-cookie";
 import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
-  CART_SAVE_SHIPPING
+  CART_SAVE_SHIPPING,
+  CART_SAVE_PAYMENT
 } from "../constants/cartConstants";
 
 const addToCart = (productId, qty) => async (dispatch, getState) => {
@@ -40,4 +41,8 @@ const saveShipping = (data) => (dispatch) => {
   dispatch({ type: CART_SAVE_SHIPPING, payload: data });
 };
 
-export { addToCart, removeFromCart, saveShipping };
+const savePayment = (data) => (dispatch) => {
+  dispatch({ type: CART_SAVE_PAYMENT, payload: data });
+};
+
+export { addToCart, removeFromCart, saveShipping, savePayment };
