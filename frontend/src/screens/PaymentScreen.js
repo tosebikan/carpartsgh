d141afb4fc3755c2793c5ff3bc96f190795aa2bb
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-03 10:54:38 +0000

    payment screen added

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { savePayment } from "../actions/cartActions";
+import CheckoutSteps from "../components/CheckoutSteps";
+
+const PaymentScreen = (props) => {
+  const [paymentMethod, setPaymentMethod] = useState("");
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePayment(paymentMethod));
+    props.history.push("placeorder");
+  };
+  return (
+    <div>
+      <CheckoutSteps steps1 steps2 steps3></CheckoutSteps>
+      <div className="form">
+        <form onSubmit={submitHandler}>
+          <ul className="form-container">
+            <li>
+              <h2>Payment</h2>
+            </li>
+            <li>
+              <div>
+                <input
+                  type="radio"
+                  name="paymentMethod"
+                  id="paymentMethod"
+                  value="paypal"
+                  onChange={(e) => setPaymentMethod(e.target.value)}
+                />
+                <label htmlFor="paymentMethod">PayPal</label>
+              </div>
+            </li>
+            <li>
+              <div>
+                <input
+                  type="radio"
+                  name="paymentMethod"
+                  id="paymentMethod"
+                  value="paystack"
+                  onChange={(e) => setPaymentMethod(e.target.value)}
+                />
+                <label htmlFor="paymentMethod">PayStack</label>
+              </div>
+            </li>
+
+            <li>
+              <button className="button primary" type="submit">
+                Continue
+              </button>
+            </li>
+          </ul>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentScreen;
