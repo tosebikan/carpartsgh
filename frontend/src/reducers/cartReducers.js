f008a9efb1038a053f0a7d7668d043c92fa8209d
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-03 10:55:08 +0000

    save payment reducer added

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,7 +1,8 @@
 import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
-  CART_SAVE_SHIPPING
+  CART_SAVE_SHIPPING,
+  CART_SAVE_PAYMENT
 } from "../constants/cartConstants";
 
 const cartReducer = (state = { cartItems: [] }, action) => {
@@ -23,6 +24,11 @@ const cartReducer = (state = { cartItems: [] }, action) => {
       };
     case CART_SAVE_SHIPPING:
       return { ...state, shipping: action.payload };
+    case CART_SAVE_PAYMENT:
+      return {
+        ...state,
+        payment: action.payload
+      };
     default:
       return state;
   }
