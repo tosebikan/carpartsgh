1e2e2bbb51c935707760c48d2d59dca9c053f746
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-23 15:04:24 +0000

    add order router

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,32 @@
+import express from "express";
+import Order from "../models/orderModel";
+import { isAuth } from "../utils";
+
+const router = express.Router();
+
+router.get("/:id", isAuth, async (req, res) => {
+  const order = await Order.findOne({ _id: req.params.id });
+  if (order) {
+    res.send(order);
+  } else {
+    res.status(404).send("order not found");
+  }
+});
+
+router.post("/", isAuth, async (req, res) => {
+  const newOrder = new Order({
+    orderItems: req.body.orderItems,
+    user: req.user._id,
+    shipping: req.body.shipping,
+    payment: req.body.payment,
+    itemsPrice: req.body.itemsPrice,
+    taxPrice: req.body.taxPrice,
+    shippingPrice: req.body.shippingPrice,
+    totalPrice: req.body.totalPrice
+  });
+
+  const newOrderCreated = await newOrder.save();
+  res.status(201).send({ message: "New Order Created", data: newOrderCreated });
+});
+
+export default router;
